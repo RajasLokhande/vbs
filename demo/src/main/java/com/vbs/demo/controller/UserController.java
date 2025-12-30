e59c4ccbe9e3a22ae8d1a705cbba40685d06f1bb
RajasLokhande <rajaslalit@gmail.com>
2025-12-30 09:34:25 +0530

    Second Commit

--- demo/src/main/java/com/vbs/demo/controller/UserController.java
+++ demo/src/main/java/com/vbs/demo/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(value = "*")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     UserRepo userRepo;
